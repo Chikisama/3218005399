e19d1855a7000047d16b3bda59d8bd4db8f00f75
Chikisama <49758343+Chikisama@users.noreply.github.com>
2020-09-24 13:38:55 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import jieba #结巴分词
-import gensim #自然语言处理库 转换成向量
 import re #正则表达式库
-import collections #词频统计库
- 
- #读取文本文件
-def read(path):
+import math
+
+#读取文本文件
+def read_text(path):
     data = ''
     file = open(path, 'r', encoding='UTF-8') #只读
     line = file.readline()
@@ -13,38 +12,58 @@ def read(path):
         line = file.readline()
     file.close()
     return data
- 
+
  #文本清洗+分词
 def text_clean(data):
     newtext = []
     pattern = re.compile(u"[^a-zA-Z0-9\u4e00-\u9fa5]")   #定义正则表达式匹配模式
-    data = pattern.sub("",data)  # 只保留英文a-zA-z、数字0-9和中文\u4e00-\u9fa5的结果。
-    newtext = jieba.lcut(data)  #分词
+    data = pattern.sub("",data)  # 只保留英文a-zA-z、数字0-9和中文\u4e00-\u9fa5的结果。	去除标点符号
+    newtext = [i for i in jieba.cut(data, cut_all=False) if i != '']  #分词
     return newtext
 
+#列出所有词，将listA和listB放在一个set中
+#余弦公式计算文本相似度
+def cos(newtext1,newtext2):
+	word_set = set(newtext1).union(set(newtext2))
+	word_dict = dict()
+	i = 0
+	for word in word_set:
+		word_dict[word] = i
+		i += 1
+	text1_cut_index = [word_dict[word] for word in newtext1]
+	text2_cut_index = [word_dict[word] for word in newtext2]
+	text1_cut_index = [0]*len(word_dict)
+	text2_cut_index = [0]*len(word_dict)
+	for word in newtext1:
+		text1_cut_index[word_dict[word]] += 1
+	for word in newtext2:
+		text2_cut_index[word_dict[word]] += 1
+	sum = 0
+	sq1 = 0
+	sq2 = 0
+	for i in range(len(text1_cut_index)):
+		sum += text1_cut_index[i] * text2_cut_index[i]
+		sq1 += pow(text1_cut_index[i], 2)
+		sq2 += pow(text2_cut_index[i], 2)
+	
+	try:
+		cos_result = round(float(sum) / (math.sqrt(sq1) * math.sqrt(sq2)), 2)
+	except ZeroDivisionError:
+		cos_result = 0.0
+	return cos_result
 
- #余弦公式计算文本相似度
-def cos_sim(text1,text2):
-    texts=[text1,text2]
-    dictionary = gensim.corpora.Dictionary(texts)
-    corpus = [dictionary.doc2bow(text) for text in texts] #只保留英文a-zA-z、数字0-9和中文\u4e00-\u9fa5的结果。
-    similarity = gensim.similarities.Similarity('-Similarity-index', corpus, num_features=len(dictionary))
-    test_corpus_1 = dictionary.doc2bow(text1)
-    cosine_sim = similarity[test_corpus_1][1]
-    return cosine_sim
- 
 if __name__ == '__main__':
-    path1 = r'C:\Users\Chiki\Desktop\homework\text\orig.txt'  #论文原文的文件的绝对路径 作业要求
-    path2 = r'C:\Users\Chiki\Desktop\homework\text\orig_add.txt'  #抄袭版论文的文件的绝对路径
-    save_path = r'C:\Users\Chiki\Desktop\homework\rate\save.txt'   #输出结果绝对路径
-    data1 = read(path1)
-    data2 = read(path2)
-    text1 = text_clean(data1)
-    text2 = text_clean(data2)
-    cos_result = cos_sim(text1, text2)
-    print("文章相似度： %.4f"%cos_result)
-    #将相似度结果写入指定文件
-    file = open(save_path, 'w', encoding="utf-8") #只写
-    file.write("文章相似度： %.4f"%cos_result)
-    file.close()
+	path1 = r'C:\Users\Chiki\Desktop\homework\text\orig.txt'  #论文原文的文件的绝对路径 作业要求
+	path2 = r'C:\Users\Chiki\Desktop\homework\text\orig_add.txt'  #抄袭版论文的文件的绝对路径
+	save_path = r'C:\Users\Chiki\Desktop\homework\rate\save.txt'   #输出结果绝对路径
+	data1 = read_text(path1)
+	data2 = read_text(path2)
+	text1 = text_clean(data1)
+	text2 = text_clean(data2)
+	cos_result = cos(text1,text2)
+	print("文章相似度： %.4f"%cos_result)
+	#将相似度结果写入指定文件
+	file = open(save_path, 'w', encoding="utf-8") #只写
+	file.write("文章相似度： %.4f"%cos_result)
+	file.close()
  
